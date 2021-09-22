fb8b7e1fe0fa42578216528b721dd577f0ce54b5
Baek2back <dumbuk12@naver.com>
2021-09-22 16:42:11 +0900

    build: dart-sass 적용 및 module.scss 반영 확인

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import "./App.css";
+import styles from "./App.module.scss";
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.App}>
       <header className="App-header">
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
