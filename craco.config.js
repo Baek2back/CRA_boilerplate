9b32ae6087de228e547bc502478d0e83b870befb
Baek2back <dumbuk12@naver.com>
2021-10-16 13:02:00 +0900

    🔧 chore: sass additional-data 추가

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,25 @@
 const CracoAlias = require("craco-alias");
 
 module.exports = {
+  style: {
+    sass: {
+      loaderOptions: (sassLoaderOptions) => {
+        return {
+          ...sassLoaderOptions,
+          additionalData: `
+            @use "sass:color";
+            @use "sass:list";
+            @use "sass:map";
+            @use "sass:math";
+            @use "sass:meta";
+            @use "sass:selector";
+            @use "sass:string";
+            @use "@/scss/_colors" as colors;
+          `,
+        };
+      },
+    },
+  },
   plugins: [
     {
       plugin: CracoAlias,
--- src/components/Test/Test.tsx
+++ src/components/Test/Test.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import styles from "@/components/Test/Test.module.scss";
 
 export default function Test() {
-  return <div>Test</div>;
+  return <div className={`${styles.Test}`}>Test</div>;
 }
